6f48870f6302588fb1ba6bc9a6312fd137f91736
Allen Yin <yinhaonan55@gmail.com>
2018-04-15 04:48:31 -0700

    allen

--- allen.js
+++ allen.js
@@ -22,8 +22,10 @@ function closestBusStop(request, response) {
                 rp(options)
                 .then(response => {
                     const the_stop = JSON.parse(response)[0];
-                    app.tell(app.buildRichResponse()
-                        .addSuggestionLink(`Bus stop at ${the_stop.name}`, `https://www.google.com/maps/@?api=1&map_action=map&center=${the_stop.latitude},${the_stop.longitude}&zoom=12`))
+                    app.tell(app
+                        .buildRichResponse()
+                        .addSimpleResponse(`Bus stop at ${the_stop.name}`)
+                        .addSuggestionLink("Google map", `https://www.google.com/maps/@?api=1&map_action=map&center=${the_stop.latitude},${the_stop.longitude}&zoom=12`))
                 })
                 .catch(err => app.tell('Sorry, I could not find any bus stops near you.'));
             }
