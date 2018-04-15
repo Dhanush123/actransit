608ffd69a998a6798eb785f060f43e77eb0872dc
sang <rkooo567@google.com>
2018-04-15 08:37:01 -0700

    add status code checking

--- dhanush.js
+++ dhanush.js
@@ -12,13 +12,17 @@ function stopPredict(gBody, gRes) {
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     var msg = "";
-    body = JSON.parse(body);
-    for(var i = 0; i < body.length; i++) {
+    if (response.statusCode !== 200) {
+      msg = "Currently, predictions are not available for stop " + stopID + ". Please try later.";
+    } else {
+      body = JSON.parse(body);
+      for(var i = 0; i < body.length; i++) {
         var time = moment(body[i]["PredictedDeparture"]);
         msg += body[i]["RouteName"] + " will be arriving " + time.calendar() +"\n  \n";
+      }
+      console.log(JSON.stringify(body));
+      msg = msg.substring(0, 1600);
     }
-    console.log(JSON.stringify(body));
-    msg = msg.substring(0, 1600);
     gRes.json({
         speech: msg,
         displayText: msg
