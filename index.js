ee4433c43d4db0fb368f438f4220bb1b1ee8be3d
Dhanush Patel <dhanush.patel@ymail.com>
2018-04-14 22:14:01 -0700

    init2

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+
+const server = express();
+server.use(bodyParser.json());
+
+server.post('/', function (req, res) {
+    console.log('webhook request:',req.body);
+    if (req.body.result.action == "stopPredict") {
+      //call function here
+    }
+    else {
+      var speech = "An error has occured.";
+      return res.json({
+        speech: speech,
+        displayText: speech
+      });
+    }
+});
+  
+server.listen((process.env.PORT || 8000), function () {
+    console.log('Server listening');
+});
