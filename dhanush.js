338337434aa3a3d14cfdaabffda29b8850811381
Allen Yin <yinhaonan55@gmail.com>
2018-04-15 11:58:06 -0700

    allen

--- dhanush.js
+++ dhanush.js
@@ -23,7 +23,6 @@ function stopPredict(gBody, gRes) {
             console.log("got in!")
             console.log("The address " + gBody.result.parameters.address)
             stopID = stops.find(o => o.Name.includes(gBody.result.parameters.address)).StopId
-            console.log(Object.keys(stopID))
         }
         else if (gBody.result.parameters.stopID != ''){
             console.log("wrong adn " + gBody.result.parameters.stopID)
@@ -35,56 +34,54 @@ function stopPredict(gBody, gRes) {
                 displayText: "I need either stop id or the address"
             });
         }
-
-        console.log("stopID is " + stopID)
     })
     .then(() => {
-         gRes.json({
-            speech: "testing",
-            displayText: "testing"
+        console.log("stopID is " + stopID)
+
+        var options = { method: "GET",
+            url: "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=actransit&stopId="+stopID
+        };
+  
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        var msg = "";
+        if (response.statusCode !== 200) {
+          msg = "Currently, predictions are not available for stop " + stopID + ". Please try later.";
+        } else {
+          body = JSON.parse(body);
+          var predictions = []
+          if (body.predictions.length != undefined){
+            predictions = body.predictions.slice()
+          }
+          else{
+            predictions.push(body.predictions)
+          }
+
+          for(var i = 0; i < predictions.length; i++) {
+            msg += "Route " + predictions[i]["routeTitle"] + " will be arriving at "
+            for(var j = 0; j < predictions[i].direction.prediction.length; j++){
+                var time = moment.unix(predictions[i].direction.prediction[j]["epochTime"]).format('hh:mm a');
+                msg += time;
+                if(j != predictions[i].direction.prediction.length-1){
+                    msg += ", ";
+                }
+            }
+            msg += "\n\n"
+          }
+        }
+
+        gRes.json({
+            speech: msg,
+            displayText: msg
         });
+        
+      });
     })
     .catch(err => console.error(err))
 
 
     
-  //   var options = { method: "GET",
-  //   	url: "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=actransit&stopId="+stopID
-  //   };
-  
-  // request(options, function (error, response, body) {
-  //   if (error) throw new Error(error);
-  //   var msg = "";
-  //   if (response.statusCode !== 200) {
-  //     msg = "Currently, predictions are not available for stop " + stopID + ". Please try later.";
-  //   } else {
-  //     body = JSON.parse(body);
-  //     var predictions = []
-  //     if (body.predictions.length != undefined){
-  //     	predictions = body.predictions.slice()
-  //     }
-  //     else{
-  //     	predictions.push(body.predictions)
-  //     }
-
-  //     for(var i = 0; i < predictions.length; i++) {
-  //     	msg += "Route " + predictions[i]["routeTitle"] + " will be arriving at "
-  //     	for(var j = 0; j < predictions[i].direction.prediction.length; j++){
-	 //        var time = moment.unix(predictions[i].direction.prediction[j]["epochTime"]).format('hh:mm a');
-	 //        msg += time;
-	 //        if(j != predictions[i].direction.prediction.length-1){
-	 //        	msg += ", ";
-	 //        }
-  //     	}
-  //     	msg += "\n\n"
-  //     }
-  //   }
-  //   gRes.json({
-  //       speech: msg,
-  //       displayText: msg
-  //   });
-  // });
-}
+  }
 
 module.exports = {
     stopPredict: stopPredict
