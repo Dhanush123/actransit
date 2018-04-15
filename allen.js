ff4ec3a873ee15edb98dfdf793e4f0b50c19c1c7
Allen Yin <yinhaonan55@gmail.com>
2018-04-14 23:09:24 -0700

    allen

--- allen.js
+++ allen.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const router = express.Router()
-
-router.post('/', (req, res) => {
+module.exports  = (req, res) => {
 	console.log('sdf')
 	
 	const response = 'hello world'
 	res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   	res.send(JSON.stringify({ "speech": response, "displayText": response}))
-})
+}
 
-module.exports = router
--- index.js
+++ index.js
@@ -5,26 +5,29 @@ const request = require('request');
 const bus_stop = require('./allen')
 
 //custom imports
-const dhanush = require('dhanush.js');
+const dhanush = require('./dhanush');
 //end here
 
 const server = express();
 server.use(bodyParser.json());
 server.use('/bus_stop', bus_stop);
 
-// server.post('/', function (req, res) {
-//     console.log('webhook request:',req.body);
-//     if (req.body.result.action == "stopPredict") {
-//       //call function here
-//     }
-//     else {
-//       var speech = "An error has occured.";
-//       return res.json({
-//         speech: speech,
-//         displayText: speech
-//       });
-//     }
-// });
+server.post('/', function (req, res) {
+    console.log('webhook request:',req.body);
+    if (req.body.result.action == "stopPredict") {
+      //call function here
+    }
+    else if(req.body.result.action == "bus_stop"){
+        bus_stop(req, res)
+    }
+    else {
+      var speech = "An error has occured.";
+      return res.json({
+        speech: speech,
+        displayText: speech
+      });
+    }
+});
   
 server.listen((process.env.PORT || 8000), function () {
     console.log('Server listening');
