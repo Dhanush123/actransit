8fb2a9817b267a0776bdea909463cd3dad0eac93
Allen Yin <yinhaonan55@gmail.com>
2018-04-15 11:28:06 -0700

    allen

--- dhanush.js
+++ dhanush.js
@@ -13,11 +13,11 @@ function stopPredict(gBody, gRes) {
 	var stops = []
 
 	rp(op1)
-    	.then(response => {
-    		stops = JSON.parse(response)
-    		console.log("The stops are " + stops)
-    	})
-    	.catch(err => console.error(err))
+    .then(response => {
+        stops = JSON.parse(response)
+        console.log("The stops are " + stops)
+    })
+    .catch(err => console.error(err))
 
 
     var stopID = null;
@@ -33,49 +33,43 @@ function stopPredict(gBody, gRes) {
 	        displayText: "I need either stop id or the address"
 	    });
     }
-
-    gRes.json({
-	        speech: "testing",
-	        displayText: "testing"
-	    });
-
     
-  //   var options = { method: "GET",
-  //   	url: "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=actransit&stopId="+stopID
-  //   };
+    var options = { method: "GET",
+    	url: "http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=actransit&stopId="+stopID
+    };
   
-  // request(options, function (error, response, body) {
-  //   if (error) throw new Error(error);
-  //   var msg = "";
-  //   if (response.statusCode !== 200) {
-  //     msg = "Currently, predictions are not available for stop " + stopID + ". Please try later.";
-  //   } else {
-  //     body = JSON.parse(body);
-  //     var predictions = []
-  //     if (body.predictions.length != undefined){
-  //     	predictions = body.predictions.slice()
-  //     }
-  //     else{
-  //     	predictions.push(body.predictions)
-  //     }
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    var msg = "";
+    if (response.statusCode !== 200) {
+      msg = "Currently, predictions are not available for stop " + stopID + ". Please try later.";
+    } else {
+      body = JSON.parse(body);
+      var predictions = []
+      if (body.predictions.length != undefined){
+      	predictions = body.predictions.slice()
+      }
+      else{
+      	predictions.push(body.predictions)
+      }
 
-  //     for(var i = 0; i < predictions.length; i++) {
-  //     	msg += "Route " + predictions[i]["routeTitle"] + " will be arriving at "
-  //     	for(var j = 0; j < predictions[i].direction.prediction.length; j++){
-	 //        var time = moment.unix(predictions[i].direction.prediction[j]["epochTime"]).format('hh:mm a');
-	 //        msg += time;
-	 //        if(j != predictions[i].direction.prediction.length-1){
-	 //        	msg += ", ";
-	 //        }
-  //     	}
-  //     	msg += "\n\n"
-  //     }
-  //   }
-  //   gRes.json({
-  //       speech: msg,
-  //       displayText: msg
-  //   });
-  // });
+      for(var i = 0; i < predictions.length; i++) {
+      	msg += "Route " + predictions[i]["routeTitle"] + " will be arriving at "
+      	for(var j = 0; j < predictions[i].direction.prediction.length; j++){
+	        var time = moment.unix(predictions[i].direction.prediction[j]["epochTime"]).format('hh:mm a');
+	        msg += time;
+	        if(j != predictions[i].direction.prediction.length-1){
+	        	msg += ", ";
+	        }
+      	}
+      	msg += "\n\n"
+      }
+    }
+    gRes.json({
+        speech: msg,
+        displayText: msg
+    });
+  });
 }
 
 module.exports = {
